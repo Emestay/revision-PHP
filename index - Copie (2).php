29e79cce7d4f9d69395d6670c19ee89debe426c0
Emestay <103575914+Emestay@users.noreply.github.com>
2023-03-14 13:49:07 +0100

    Debut

---
+++ index - Copie (2).php
@@ -0,0 +1,48 @@
+<?php
+echo '  <script src="https://cdn.tailwindcss.com"></script>';
+
+$chaine = "Le chat noir dort tranquillement dans sa cage en bois, pendant que l'oiseau chante joyeusement sur la branche de l'arbre.";
+
+function remplacerCaracteres($chaine)
+{
+    $chaine = str_replace('a', '@', $chaine);
+    $chaine = str_replace('e', '3', $chaine);
+    $chaine = str_replace('i', '1', $chaine);
+    $chaine = str_replace('o', '0', $chaine);
+    //Je fais a la suite une maj sur l'autre
+    $output = "";
+    for ($i = 0; $i < strlen($chaine); $i++) {
+        $char = $chaine[$i];
+        if ($i % 2 == 0) {
+            $char = strtoupper($char);
+        }
+        $output .= $char;
+    }
+    return $output;
+}
+
+$resultat = remplacerCaracteres($chaine);
+
+echo $resultat .'<br> <br>';
+
+$tab = [12,42,36,48,55,87,1,59,48,36,73,49];
+echo array_sum($tab).'<br> <br>';
+
+
+
+$min = $tab[0];
+for ($i = 1; $i < count($tab); $i++) {
+    if ($tab[$i] < $min) {
+        $min = $tab[$i];
+    }
+}
+echo $min;
+
+foreach($tab as $temp ){
+    if($temp < $min){
+        $min = $temp;
+    }
+}
+echo $min;
+
+echo min($tab);
